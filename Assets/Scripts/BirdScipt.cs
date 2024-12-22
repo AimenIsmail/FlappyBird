2b45ba02a714c84ad09023f0baafca3d0e67c150
orangesareblack <malikharoonkhokhar789@gmail.com>
2024-12-22 16:27:38 +0500

    Tap Button and pipe movement

--- Assets/Scripts/BirdScipt.cs
+++ Assets/Scripts/BirdScipt.cs
@@ -1,47 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BirdScipt : MonoBehaviour
 {
-    public float velocity = 1;
+    public float velocity = 10;
     private Rigidbody2D rb;
     public static int scoreNumber = 0;
-    public TextMeshProUGUI inGameScoreText;
-    public GameObject gameOverCanvas;
+    //public TextMeshProUGUI inGameScoreText;
+    //public GameObject gameOverCanvas;
     public Animator birdAnim;
+    public Button TapButton;
     // Start is called before the first frame update
     void Start()
     {
         scoreNumber = 0;
         rb = GetComponent<Rigidbody2D>();
         Time.timeScale = 1;
-        
+	    
     }
 
     // Update is called once per frame
-    void Update()
+   public void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            birdAnim.Play("BirdFlap");
-            rb.velocity = Vector2.up * velocity;
-        }
-        inGameScoreText.text = scoreNumber.ToString();
+        Button btn = TapButton.GetComponent<Button>();
+       btn.onClick.AddListener(TaskOnClick);
+       // inGameScoreText.text = scoreNumber.ToString();
+    }
+    void TaskOnClick(){
+	    birdAnim.Play("BirdFlap");
+	    rb.velocity = Vector2.up * velocity;
     }
 
-    private void OnCollisionEnter2D(Collider2D collision)
+   /* private void OnCollisionEnter2D(Collider2D collision)
     {
         gameOverCanvas.SetActive(true);
         Time.timeScale = 0;
-    }
+    }*/
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    /*private void OnTriggerEnter2D(Collider2D collision)
     {
         scoreNumber++;
-    }
+    }*/
 
     public void playAgain()
     {
